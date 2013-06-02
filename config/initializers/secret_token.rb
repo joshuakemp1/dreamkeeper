f85aa8bf216e49973328757bebcc86e59a93e535
joshua kemp <joshuakemp85@gmail.com>
2013-06-01 21:02:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreamkeeper::Application.config.secret_token = '699a55566874bde6b66f716a99b61e0dbcf002390dba3ddd449bb51d25026cf0939e508306e4e7fa7f036276a4bed7f84b56641efc154170820b4d9937ca1026'
